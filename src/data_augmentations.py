7b5ad8a01e05def3800a581b19b07602c8d32c91
M'Saydez <msaydez_campbell@hotmail.com>
2023-01-29 11:38:54 +0100

    adjusted augmentation

--- src/data_augmentations.py
+++ src/data_augmentations.py
@@ -18,7 +18,11 @@ randomAugment = transforms.Compose([
     transforms.ToTensor()
 ])
 
-transformations = [transforms.Resize((224, 224)), transforms.RandomHorizontalFlip(p=0.5), transforms.ColorJitter(brightness=0.1, contrast=0.1),
-                   transforms.ToTensor()]
+transformations = [transforms.Resize((224, 224)),
+                   transforms.RandomHorizontalFlip(p=0.5),
+                   transforms.RandomVerticalFlip(p=0.5),
+                   transforms.RandomRotation(30),
+                   transforms.ToTensor(),
+                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
 
 
