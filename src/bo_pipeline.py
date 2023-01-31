b357eb9b55b88d387eaede9d4d4db580d577d2f2
domiomi3 <matus.dominika@gmail.com>
2023-01-31 14:57:28 +0100

    Add dropout to search space

--- src/bo_pipeline.py
+++ src/bo_pipeline.py
@@ -16,6 +16,7 @@ def get_pipeline_space() -> dict:
         learning_rate=neps.FloatParameter(lower=1e-3, upper=0.1, log=True),
         weight_decay=neps.FloatParameter(lower=1e-5, upper=1e-2, log=True),
         momentum=neps.FloatParameter(lower=0, upper=0.8),
+        dropout=neps.FloatParameter(lower=0.0, upper=0.5),
         cutmix_prob=neps.FloatParameter(lower=0.5, upper=0.9),
         beta=neps.FloatParameter(lower=0, upper=0.5)
     )
